c59e3a363008f82cf92f5d88f4b0839ad6ab03e2
Onam Chilwan <OnamC@asos.com>
2021-01-15 16:36:42 +0000

    refactored tests to one but with multiple test cases

--- PotterKata/DiscountTests.cs
+++ PotterKata/DiscountTests.cs
@@ -6,22 +6,14 @@ namespace PotterKata
     [TestFixture]
     public class DiscountTests
     {
-        [Test]
-        public void Given_Two_Books_When_Determining_Discount_Rate_Then_Five_Percent_Discount_Is_Returned()
+        [TestCase(2, 0.95)]
+        [TestCase(3, 0.90)]
+        [TestCase(4, 0.80)]
+        public void Given_X_Books_When_Determining_Discount_Rate_Then_The_Correct_Discount_Is_Returned(int numberOfBooks, double expectedDiscount)
         {
-            const int numberOfBooks = 2;
             var discount = Discount.Create(numberOfBooks);
             
-            Assert.That(discount.Rate, Is.EqualTo(0.95));
-        }
-        
-        [Test]
-        public void Given_Three_Books_When_Determining_Discount_Rate_Then_Ten_Percent_Discount_Is_Returned()
-        {
-            const int numberOfBooks = 3;
-            var discount = Discount.Create(numberOfBooks);
-            
-            Assert.That(discount.Rate, Is.EqualTo(0.90));
+            Assert.That(discount.Rate, Is.EqualTo(expectedDiscount));
         }
     }
 }
