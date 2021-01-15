0d70a730c2256c2f71f6f8b7801261c145393435
Onam Chilwan <OnamC@asos.com>
2021-01-15 14:38:49 +0000

    Failing test for adding multiple different books. The expectation is so that we have one book set and two books within it.
    I have opted to tackle it from this perspective as I found this to be the troublesome part of the kata. The actual discount logic is simple.

--- PotterKata/BasketTests.cs
+++ PotterKata/BasketTests.cs
@@ -11,22 +11,33 @@ namespace PotterKata
         private Basket _subject;
 
         [Test]
-        public void AddingItemsToBasket()
+        public void AddingSingleBookToBasket()
         {
             this.Given(_ => An_Empty_Basket())
-                .When(_ => Book_Is_Added_To_Basket(new Book()))
+                .When(_ => Book_Is_Added_To_Basket(new Book(HarryPotterSeries.Philosophers_Stone)))
                 .Then(_ => Basket_Is_Not_Empty())
+                .BDDfy();
+        }
+        
+        [Test]
+        public void AddingSameBookTwiceToBasket()
+        {
+            this.Given(_ => An_Empty_Basket())
+                .When(_ => Book_Is_Added_To_Basket(new Book(HarryPotterSeries.Philosophers_Stone)))
+                .And(_ => Book_Is_Added_To_Basket(new Book(HarryPotterSeries.Philosophers_Stone)))
+                .Then(_ => Basket_Is_Not_Empty())
+                .Then(_ => Basket_Contains_X_Number_Of_Items(2))
                 .BDDfy();
         }
 
         [Test]
-        public void AddingMultipleItemsToBasket()
+        public void AddingMultipleDifferentBooksToBasket()
         {
-            var books = new List<Book> { new Book(), new Book() };
+            var books = new List<Book> { new Book(HarryPotterSeries.Philosophers_Stone), new Book(HarryPotterSeries.Chamber_of_Secrets) };
             this.Given(_ => An_Empty_Basket())
                 .When(_ => Books_Are_Added_To_Basket(books))
                 .Then(_ => Basket_Is_Not_Empty())
-                .Then(_ => Basket_Contains_X_Number_Of_Items(2))
+                .Then(_ => Basket_Contains_X_Number_Of_Items(1))
                 .BDDfy();
         }
 
@@ -56,8 +67,25 @@ namespace PotterKata
         }
     }
 
+    public enum HarryPotterSeries
+    {
+        Philosophers_Stone = 1,
+        Chamber_of_Secrets = 2,
+        Prisoner_of_Azkaban = 3,
+        Goblet_of_Fire = 4,
+        Order_of_the_Phoenix = 5,
+        Half_Blood_Prince = 6,
+        Deathly_Hallows = 7
+    }
+    
     public class Book
     {
+        public HarryPotterSeries Series { get; }
+
+        public Book(HarryPotterSeries series)
+        {
+            Series = series;
+        }
     }
 
     public class Basket
