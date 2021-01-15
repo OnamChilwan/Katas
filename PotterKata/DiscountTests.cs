f5a6c85cb2b0685d2a61ae29cfef8fd00c3a4c25
Onam Chilwan <OnamC@asos.com>
2021-01-15 16:25:21 +0000

    Started working on discount rates to plug in. Failing test committed for determining the discount for 2 books.

---
+++ PotterKata/DiscountTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using PotterKata.Models;
+
+namespace PotterKata
+{
+    [TestFixture]
+    public class DiscountTests
+    {
+        [Test]
+        public void Given_Two_Books_When_Determining_Discount_Rate_Then_Five_Percent_Discount_Is_Returned()
+        {
+            const int numberOfBooks = 2;
+            var discount = Discount.Create(numberOfBooks);
+            
+            Assert.That(discount.Rate, Is.EqualTo(0.95));
+        }
+    }
+}
---
+++ PotterKata/Models/Discount.cs
@@ -0,0 +1,17 @@
+﻿namespace PotterKata.Models
+{
+    public class Discount
+    {
+        private Discount(double rate)
+        {
+            this.Rate = rate;
+        }
+        
+        public static Discount Create(int numberOfBooks)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public double Rate { get; }
+    }
+}
