51960543810a8a27671563be868596a53bdf8bd0
Onam Chilwan <OnamC@asos.com>
2021-01-15 16:31:02 +0000

    Tests failing for 10 percent discount

--- PotterKata/DiscountTests.cs
+++ PotterKata/DiscountTests.cs
@@ -14,5 +14,14 @@ namespace PotterKata
             
             Assert.That(discount.Rate, Is.EqualTo(0.95));
         }
+        
+        [Test]
+        public void Given_Three_Books_When_Determining_Discount_Rate_Then_Ten_Percent_Discount_Is_Returned()
+        {
+            const int numberOfBooks = 3;
+            var discount = Discount.Create(numberOfBooks);
+            
+            Assert.That(discount.Rate, Is.EqualTo(0.90));
+        }
     }
 }
